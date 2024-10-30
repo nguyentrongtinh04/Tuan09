b4621444e4dc590aac1700115fff99ee7ca02230
NguyenTrongTinhh <nguyentrongtinh4803@gmail.com>
2024-10-30 11:08:34 +0700

    Xong câu 2 redux_toolkit

--- bt_tuan09/App.js
+++ bt_tuan09/App.js
@@ -1,6 +1,30 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import API_Screen_01 from './Screen/API_Screen_01';
+// import API_Screen_02 from './Screen/API_Screen_02';
+// import API_Screen_03 from './Screen/API_Screen_03';
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="API_Screen_01">
+//         <Stack.Screen name="API_Screen_01" component={API_Screen_01} options={{ headerShown: false }} />
+//         <Stack.Screen name="API_Screen_02" component={API_Screen_02} options={{ headerShown: false }} />
+//         <Stack.Screen name="API_Screen_03" component={API_Screen_03} options={{ headerShown: false }} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { store } from './Screen/Store';
 import API_Screen_01 from './Screen/API_Screen_01';
 import API_Screen_02 from './Screen/API_Screen_02';
 import API_Screen_03 from './Screen/API_Screen_03';
@@ -9,12 +33,14 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="API_Screen_01">
-        <Stack.Screen name="API_Screen_01" component={API_Screen_01} options={{ headerShown: false }} />
-        <Stack.Screen name="API_Screen_02" component={API_Screen_02} options={{ headerShown: false }} />
-        <Stack.Screen name="API_Screen_03" component={API_Screen_03} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="API_Screen_01">
+          <Stack.Screen name="API_Screen_01" component={API_Screen_01} options={{ headerShown: false }} />
+          <Stack.Screen name="API_Screen_02" component={API_Screen_02} options={{ headerShown: false }} />
+          <Stack.Screen name="API_Screen_03" component={API_Screen_03} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ bt_tuan09/Screen/jobSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchJobs = createAsyncThunk('jobs/fetchJobs', async () => {
+  const response = await axios.get('https://67219f5898bbb4d93ca901a3.mockapi.io/task');
+  return response.data;
+});
+
+export const addJob = createAsyncThunk('jobs/addJob', async (title) => {
+  const response = await axios.post('https://67219f5898bbb4d93ca901a3.mockapi.io/task', { title });
+  return response.data;
+});
+
+export const editJob = createAsyncThunk('jobs/editJob', async ({ id, title }) => {
+  const response = await axios.put(`https://67219f5898bbb4d93ca901a3.mockapi.io/task/${id}`, { title });
+  return response.data;
+});
+
+const jobSlice = createSlice({
+  name: 'jobs',
+  initialState: {
+    list: [],
+    status: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchJobs.fulfilled, (state, action) => {
+        state.list = action.payload;
+        state.status = 'success';
+      })
+      .addCase(addJob.fulfilled, (state, action) => {
+        state.list.push(action.payload);
+      })
+      .addCase(editJob.fulfilled, (state, action) => {
+        const index = state.list.findIndex(job => job.id === action.payload.id);
+        if (index !== -1) state.list[index] = action.payload;
+      });
+  },
+});
+
+export default jobSlice.reducer;
